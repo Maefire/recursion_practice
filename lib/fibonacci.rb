c2af5ddd3499de7276521454796ef97d11b8e7a5
Maefire <masked_phoenix@yahoo.com>
2023-03-31 02:27:53 -0500

    Add recursive method for fibonacci
    Made a recursive method so fibonacci is returned up to the nth number

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,4 +1,4 @@
-def fib(int)
+def fibs(int)
   return result if int.eql?(0)
 
   result = []
@@ -16,5 +16,15 @@ def fib(int)
   end
   result
 end
-p fib(8)
-[0, 1, 1, 2, 3, 5, 8, 13]
+p fibs(8)
+# [0, 1, 1, 2, 3, 5, 8, 13]
+
+def fibs_rec(n)
+  return [0] if n.eql?(0)
+  return [0, 1] if n.eql?(1)
+
+  results = fibs_rec(n - 1)
+  results << results[-1] + results[-2]
+  results[0...n]
+end
+p fibs_rec(8)
